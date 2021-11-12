583869f3e870d3bb9e6e4bb1147d8db2a1141029
Claudio Landerer <landerer.claudio@gmail.com>
2021-11-12 08:04:08 +0100

    Student-Domain-Klasse hinzugefügt, StudentRepo definiert, Application-Runner in Betrieb genommen für Dummy-Daten

--- src/main/java/at/itkollegimst/studentenverwaltung/StudentenverwaltungApplication.java
+++ src/main/java/at/itkollegimst/studentenverwaltung/StudentenverwaltungApplication.java
@@ -1,13 +1,27 @@
 package at.itkollegimst.studentenverwaltung;
 
+import at.itkollegimst.studentenverwaltung.domain.Student;
+import at.itkollegimst.studentenverwaltung.repositories.StudentJPARepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentenverwaltungApplication {
+public class StudentenverwaltungApplication implements ApplicationRunner {
+
+	@Autowired
+	StudentJPARepo studentJPARepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentenverwaltungApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		this.studentJPARepo.save(new Student("Claudio Landerer","6460"));
+		this.studentJPARepo.save(new Student("Günter Hasel","3322"));
+		this.studentJPARepo.save(new Student("Maria Brunsteiner","8080"));
+	}
 }
